399036aa78fffc85d96e68840595f65dfe2b3e60
Medha Kuchimanchi <mkuchi@unc.edu>
2025-11-18 17:08:40 -0500

    Initial Commit

---
+++ Final_Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Final_Project
+//
+//  Created by Medha Kuchimanchi on 11/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Final_Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final_Project
+//
+//  Created by Medha Kuchimanchi on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
